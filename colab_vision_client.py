ea02be9d5c18697984a684bef94de8d06c106a4a
nbovee <12849851+nbovee@users.noreply.github.com>
2022-06-09 19:05:52 -0400

    progress

--- colab_vision_client.py
+++ colab_vision_client.py
@@ -1,22 +1,28 @@
+from doctest import testfile
 import os
 import ColabVision.src.colab_vision as cv
 
 def demo_funct():
-    client = cv.FileClient('localhost:8888')
+    client = cv.FileClient('localhost:8892')
 
     # demo for file uploading
     in_file_name = './tmp/morbius_in.jpg'
-    print(os.path.exists(in_file_name))
-    client.upload(in_file_name)
-
+    # print(os.path.exists(in_file_name))
+    response = client.upload(in_file_name)
+    # print(response)
     # demo for file downloading:
     out_file_name = './tmp/morbius_out.jpg'
     if os.path.exists(out_file_name):
         os.remove(out_file_name)
-    client.download('whatever_name', out_file_name)
-    os.system(f'sha1sum {in_file_name}')
-    os.system(f'sha1sum {out_file_name}')
-    return out_file_name
+    test_dict = client.processingTime(response.id)
+    dl_time_s = cv.timer()
+    client.download(response.id, out_file_name)
+    test_dict['download'] = cv.timer() - dl_time_s
+    # os.system(f'sha1sum {in_file_name}')
+    # os.system(f'sha1sum {out_file_name}')
+    # print(test_dict)
+    return test_dict
+    
 
 if __name__ == '__main__':
     _ = demo_funct()
