621f42e017daea3d85e0919948e8baa41297dc1d
nbovee <12849851+nbovee@users.noreply.github.com>
2022-10-09 18:20:05 -0400

    progress

--- build_grpc.py
+++ build_grpc.py
@@ -13,4 +13,4 @@ protoc.main((
 
 # (.venv) nick@DESKTOP-HF7K570:~/collaborative-vision-research/ColabVision/protos$ 
 # python -m grpc_tools.protoc -I. --python_out=../src --grpc_python_out=../src ./colab_vision.proto
-# python3 -m grpc_tools.protoc -I./ColabVision/protos --python_out=./ColabVision/src/colab_vision_pb2 --grpc_python_out=./ColabVision/src ./ColabVision/protos/colab_vision.proto
+# python -m grpc_tools.protoc -I./ColabVision/protos --python_out=./ColabVision/src/colab_vision_pb2 --grpc_python_out=./ColabVision/src ./ColabVision/protos/colab_vision.proto
