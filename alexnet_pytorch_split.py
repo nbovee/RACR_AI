9e88663a4a616edb5fbb12fa6857b364358398ee
nbovee <12849851+nbovee@users.noreply.github.com>
2022-10-16 22:54:13 -0400

    prog

--- alexnet_pytorch_split.py
+++ alexnet_pytorch_split.py
@@ -1,15 +1,9 @@
 import numpy as np
 from PIL import Image
 import torch
+import torch.nn as nn
 from torchvision import transforms, models
 
-# selected = "MobileNet"
-# if selected == "AlexNet":
-#     selected ="alexnet"
-# elif selected == "SqueezeNet":
-#     selected= "squeezenet1_1"
-# elif selected == "MobileNet":
-#     selected ="mobilenet_v3s"
 image_size = (224, 224)
 # model = torch.hub.load('pytorch/vision:v0.10.0', selected, pretrained=True)
 model = models.alexnet(pretrained=True)
@@ -22,6 +16,48 @@ preprocess = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
+
+class SplitAlex(models.AlexNet):
+
+    # almost exactly like pytorch AlexNet, but we cannot split out of a Sequential
+    def __init__(self, num_classes: int = 1000, dropout: float = 0.5) -> None:
+        super().__init__() # have to do this to get some stuff out of the way.
+        # _log_api_usage_once(self) #idk what this is
+
+        # we still need weights but that actually doesnt affect our test cases at the moment
+        self.features = [
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        ]
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = [
+            nn.Dropout(p=dropout),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        ]
+
+    def forward(self, x: torch.Tensor, start_layer = 0, end_layer = 9999) -> torch.Tensor:
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
 
 class Model:
     def __init__(self,) -> None:
