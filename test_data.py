5dc6d0e68df2d2d56036eca515dc14d37f4f1bc8
nbovee <12849851+nbovee@users.noreply.github.com>
2022-10-19 06:51:43 +0000

    merged working copies

--- test_data.py
+++ test_data.py
@@ -2,25 +2,30 @@ from data_wrapper import data_wrapper
 from PIL import Image
 import glob
 
-class test_data_loader(data_wrapper):
+class test_data_loader:
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self):
         self.image_list = []
-        self.load_data("test/*");
+        self.load_data("test/*.JPEG")
+    
 
     def has_next(self):
         return bool(len(self.image_list))
 
     def next(self):
-        val = self.image_list.pop(-1)
+        if self.has_next() is not None:
+            [val, filename] = self.image_list.pop()
+        else:
+            return
         for i in range(1,22): # hardcoded split layers for AlexNet - no full processing yet
-            yield [ val, i ]
+            print(f"yield split layer {i} remaining images {len(self.image_list)}")
+            yield [ val.load(), i, filename ]
 
     def load_data(self, path):
         max_images = 2
+        # print(f"len iglob {len(list(glob.iglob(path)))}")
         for image in glob.iglob(path):
             print(image)
-            self.image_list.append(Image.Image.open(image))
+            self.image_list.append([ Image.open(image), image ])
             if len(self.image_list) >= max_images:
                 break
