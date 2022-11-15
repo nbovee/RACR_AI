e4ea81b069ab13df1e4a883cb84c51ab9a47305c
nbovee <12849851+nbovee@users.noreply.github.com>
2022-11-15 04:04:59 +0000

    debug pass

--- src/colab_vision_server.py
+++ src/colab_vision_server.py
@@ -35,6 +35,7 @@ class FileServer(colab_vision_pb2_grpc.colab_visionServicer):
                 #unpack msg contents
                 current_chunks = []
                 last_id = None
+                reference_time = 0
                 for i, msg in enumerate(request_iterator):
                     # print(f"Message received with id {msg.id}.")
                     m = colab_vision_pb2.Response_Dict(
@@ -48,10 +49,12 @@ class FileServer(colab_vision_pb2_grpc.colab_visionServicer):
                         raise Exception("Hard Exit called")
                     # if new id
                     if colab_vision_pb2.ACT_RESET in msg.action:
+                        reference_time = np.float32(time.time())
                         m.keypairs.clear()
                         current_chunks = []
                         last_id = msg.id
-                        m.keypairs["server_start_time"] = time.time()
+                        m.keypairs["server_reference_float"] = reference_time
+                        m.keypairs["server_start_time"] = time.time()-reference_time
                     # rebuild data
                     if msg.id == last_id and colab_vision_pb2.ACT_INFERENCE not in msg.action:
                         current_chunks.append(msg.chunk)
@@ -61,15 +64,15 @@ class FileServer(colab_vision_pb2_grpc.colab_visionServicer):
                         print(f"Chunks to convert: {len(current_chunks)}")
                         current_chunks = colab_vision.save_chunks_to_object(current_chunks)
                         # print(current_chunks[-27:-2].hex()) # investigate
-                        m.keypairs["server_assemble_time"] = time.time()
+                        m.keypairs["server_assemble_time"] = time.time()-reference_time
                         # decompress if needed
                         if colab_vision_pb2.ACT_COMPRESSED in msg.action:
                             current_chunks = blosc.unpack_tensor(current_chunks)
-                            m.keypairs["server_decompression_time"] = time.time() #not sure if this can even be done on instantiation
+                            m.keypairs["server_decompression_time"] = time.time()-reference_time #not sure if this can even be done on instantiation
                         # start inference
                         prediction = self.model.predict(current_chunks, start_layer=msg.layer)
                         m.results = prediction.encode()
-                        m.keypairs["server_inference_time"] = time.time()
+                        m.keypairs["server_inference_time"] = time.time()-reference_time
                         # clean results
                         # m.keypairs["server_processing_time"] = time.time()
                     yield m
