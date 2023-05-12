2ead240a2ec2b0e3f7032839fb59dde857508517
S Piccolo <stevepic95@gmail.com>
2023-05-12 17:42:58 -0400

    pushing current changes to source before leaving for wedding

--- README.md
+++ README.md
@@ -17,3 +17,26 @@ custom install of <https://developer.download.nvidia.com/compute/cuda/repos/ubun
 <https://paperswithcode.com/method/yolov4>
 <>
 
+
+### Changes introduced in the RPi3 Compatibility Feature Branch
+* A few utility scripts have been added to make it easier to interface with the IoT devices (RPi3s, Jetson Nanos) from a PC
+	* open_gateway.sh: Allows the IoT devices in the CS Lab LAN to access the internet using your PC as a gateway. Helpful for installing new software or updates.
+		* Must connect your PC to the LAN via ethernet and set your ethernet adaptor to use the static IP 192.168.1.200. I'm also pretty sure you need to be using a Debian-based OS.
+	* sync_repos.sh: Quickly syncs changes made to the collab-vision repo on your local machine to all IoT devices in the LAN.
+		* Must have passwordless (pub/private key) ssh set up for each IoT device. Use ssh-keygen to get this set up quickly.
+	* deploy_node.sh: Deploys a containerized "node" for testing as either a remote or client device. Pass option flags to determine which base image to use, depending on the architecture and hardware of the node.
+		* There are a few options - check the source code for a longer description comment
+		* Also gives you the ability to run any image in "Interactive Terminal" mode so you can use a terminal from inside the running container, test, and poke around for troubleshooting.
+* Docker Image tarballs are being saved directly in the repo
+	* Makes it a little easier to ensure each device has access to the same images
+	* Using docker load command to load images from the tarballs if they are not already present in the local system (this happens in deploy_node.sh)
+* The process for building containers with new functionality has changed a bit
+	* The base images simply provide the same clean slate (with Python 3.7, torch 1.10, torchvision 0.11, etc.) for multiple architectures (arm64 with cuda, arm64 without cuda, x86 with and without).
+	* To specify the additional setup and final execution for a new container, create a new Dockerfile that starts from a base image, sets up what it has to, then executes the python program you want to run.
+		* The suffix you choose for the Dockerfile will be used to tag this image for future use, so try to be descriptive.
+		* Like before, the Dockerfile will still accept a "branch" argument to implement remote or client functionality in the same way.
+* I have begun working on a "local_testing" directory for roughly testing new approaches on a virtual bridge network so we can see how things work without actually having physical access to the LAN in the CS lab.
+	* There is a docker-compose.yml file that doesn't work as intended (yet).
+* I added a text file named "device_info" to the .gitignore file since it contains semi-private information about device IPs and passwords, but the info has been posted in the RACR discord.
+* We are successfully running PyTorch and torchvision on the Raspberry Pi 3 devices inside a docker container.
+
--- scripts/deploy_node.sh
+++ scripts/deploy_node.sh
@@ -5,20 +5,39 @@
 # Docker container running on either a Jetson, Raspberry Pi, or PC, which
 # communicates with other nodes on the network using the gRPC protocol.
 #
+# USAGE:
+#			./deploy_node [FLAGS] SUFFIX
+#
 # FLAGS:
 # 	-n		"no CUDA"	Use for hosts without CUDA support (like the Pi 3)
 #	-r		"remote"	Deploys a remote/server host (rather than client)
+#	-a		"arm64"		Use for arm64 hosts (default is x86)
+#	-t		"terminal"	Opens an interactive terminal session inside the container
+#
+# SUFFIX
+#			The suffix is a required argument that tells the script how to find
+#			which Dockerfile to use from the Dockerfiles directory. (The only
+#			difference between the filenames is the suffix).
+#
+#			The base image has the correct Python and PyTorch dependencies installed;
+#			all that's really left to do is specify the script you'd like to execute
+#			when the container is run, including any extra setup you want to include
+#			in the Dockerfile before that.
+#
+#			When choosing a suffix for your Dockerfile, try to make it descriptive
+#			as it will be used to tag the final image as well.
 
-# Default values if no flags are set
+# Default values if no flags are set (a client machine with CUDA and x86 arch)
 CUDA_STATE="cuda"
 BUILD_ARG="client"
 ARCH="x86"
+TERMINAL="false"
 
 # parse through flags
 while getopts ":nr" opt; do
   case $opt in
     n)
-      CUDA_STATE="no-cuda"
+      CUDA_STATE="nocuda"
       ;;
     r)
       BUILD_ARG="remote"
@@ -26,6 +45,10 @@ while getopts ":nr" opt; do
 	a)
       ARCH="arm64"
       ;;
+	t)
+	  TERMINAL="true"
+	  ;;
+	  
     \?)
       echo "Invalid option: -$OPTARG" >&2
       exit 1
@@ -33,23 +56,48 @@ while getopts ":nr" opt; do
   esac
 done
 
-# Define the image name
-IMAGE_NAME="colab-vision-$BUILD_ARG-$CUDA_STATE"
+# Remove the processed options
+shift $((OPTIND-1))
 
-# Get the name of the right Dockerfile to build with
-DOCKERFILE="Dockerfile.$CUDA_STATE"
-
-# Check if the image exists
-IMAGE_EXISTS=$(docker images -q $IMAGE_NAME)
-
-# Build the image if it does not exist
-if [ -z "$IMAGE_EXISTS" ]; then
-    echo "Building a $BUILD_ARG image using $DOCKERFILE..."
-    docker build -t $IMAGE_NAME -f $DOCKERFILE --build-arg branch=$BUILD_ARG .
-else
-    echo "Docker image for $BUILD_ARG already exists on this machine, skipping build."
+# Check if a suffix was provided
+if [ $# -eq 0 ]; then
+    echo "No Dockerfile suffix provided. Please provide a suffix as a non-option argument."
+    exit 1
 fi
 
-# Run the container with the appropriate port mapping
-docker run -p 3001:3000 --name "$IMAGE_NAME-instance" $IMAGE_NAME
+# Save the suffix
+SUFFIX="$1"
+
+# TODO: The script should check the machine's CUDA_STATE and ARCH itself,
+#		either giving the user a warning if things don't match up, or even
+#		making the flags unnecessary if the checking is robust enough.
+
+# Infer the base image name (right now the convention is arch-cuda_state-build_arg-base
+BASE_IMAGE_NAME="$ARCH-$CUDA_STATE-$BUILD_ARG-base"
+
+# Get the name of the right Dockerfile to build with
+DOCKERFILE="../Dockerfiles/Dockerfile.$SUFFIX"
+
+# Check if the image was preloaded
+BASE_IMAGE_PRELOADED=$(docker images -q $BASE_IMAGE_NAME)
+
+# Load the image if it was not preloaded
+if [ -z "$BASE_IMAGE_PRELOADED" ]; then
+	PATH_TO_BASE_IMAGE_TARBALL="../docker_images/$BASE_IMAGE_NAME"
+    echo "Loading the $BASE_IMAGE_NAME image using $PATH_TO_BASE_IMAGE_TARBALL..."
+	docker load -i $PATH_TO_BASE_IMAGE_TARBALL || echo "Problem loading image from tarball. Is it there?"
+else
+    echo "Base image for $BASE_IMAGE_NAME already exists on this machine, skipping load."
+fi
+
+# Build on top of the base image now    
+docker build -t "$SUFFIX-$BUILD_ARG" -f $DOCKERFILE --build-arg branch=$BUILD_ARG .
+
+# if the -t flag was passed, run the container in interactive terminal mode
+if [ "$TERMINAL" = "true" ]; then
+	docker run -p 3001:3000 -it --name "$BASE_IMAGE_NAME-instance" $BASE_IMAGE_NAME /bin/bash
+else
+	# Run the container normally and with the appropriate port mapping
+	docker run -p 3001:3000 --name "$BASE_IMAGE_NAME-instance" $BASE_IMAGE_NAME
+fi
 
