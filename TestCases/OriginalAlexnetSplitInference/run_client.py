9773748eab1cb33a15e6f818dee170d9dd024b4d
S Piccolo <stevepic95@gmail.com>
2023-06-20 14:27:13 -0400

    fix memory over-allocation on Pi 3

--- TestCases/OriginalAlexnetSplitInference/run_client.py
+++ TestCases/OriginalAlexnetSplitInference/run_client.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
         print("Usage: python3 run_client.py <IP> <port>")
         exit(1)
     client = cv.FileClient(sys.argv[1] + ":" + sys.argv[2])
+    print("Constructed client.")
     atexit.register(client.safeClose)
     test_data = data_loader()
+    print("Initialized data_loader.")
     while test_data.has_next():
         client.initiateInference(test_data)
