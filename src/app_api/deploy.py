b81933f095816fb1067a466fb9bd83a651b7f35f
Stephen <piccol24@students.rowan.edu>
2023-10-12 17:50:54 -0400

    fix zerodeploy remote script

--- src/app_api/deploy.py
+++ src/app_api/deploy.py
@@ -18,6 +18,19 @@ import atexit
 import shutil
 import logging
 import logging.handlers
+from importlib import import_module
+
+
+server_module     = "$SVR-MODULE$"
+server_class      = "$SVR-CLASS$"
+model_class       = "$MOD-CLASS$"
+model_module      = "$MOD-MODULE$"
+executor_module   = "$EXR-MODULE$"
+executor_class    = "$EXR-CLASS$"
+node_name         = "$NODE-NAME$"
+participant_host  = "$PRT-HOST$"
+observer_ip       = "$OBS-IP$"
+
 
 def setup_remote_logger(node_name, host, observer_ip):
     logger = logging.getLogger(f"{node_name}_logger")
@@ -29,7 +42,7 @@ def setup_remote_logger(node_name, host, observer_ip):
     logger.addHandler(socket_handler)
     return logger
 
-logger = setup_remote_logger("$NODE_NAME$", "$PARTICIPANT_HOST$", "$OBSERVER_IP$")
+logger = setup_remote_logger(node_name, participant_host, observer_ip)
 
 logger.info("Zero deploy sequence started. Removing __pycache__ and *.pyc files from tempdir.")
 
@@ -50,41 +63,47 @@ except Exception:
 
 sys.path.insert(0, here)
 
-logger.info("Executing remote script line 'from $MODULE$ import $SERVER$ as ServerCls'")
-from $MODULE$ import $SERVER$ as ServerCls
+logger.info(f"Importing {server_class} from {server_module} as ServerCls'")
+m = import_module(server_module)
+ServerCls = getattr(m, server_class)
 
-logger.info("Importing ParticipantService in remote script")
-from experiment_design.rpc_services.participant_service import ParticipantService
+logger.info("Importing ParticipantService from src.experiment_design.node_behavior.base.")
+from src.experiment_design.node_behavior.base import ParticipantService
 
-if "$MOD-CLASS$" and "$MOD-MODULE$":
-    logger.info("Importing $MOD-CLASS$ from experiment_design.models.$MOD-MODULE$.")
-    from experiment_design.models.$MOD-MODULE$ import $MOD-CLASS$ as Model
+if model_class and model_module:
+    logger.info(f"Importing {model_class} from src.experiment_design.models.{model_module}.")
+    m = import_module(f"src.experiment_design.models.{model_module}")
+    Model = getattr(m, model_class)
 else:
+    logger.info("Using default model (AlexNet)")
     Model = None
 
-logger.info("Importing $SCH-CLASS$ from experiment_design.runners.$SCH-MODULE$.")
-from experiment_design.runners.$SCH-MODULE$ import $SCH-CLASS$ as Runner
+logger.info(f"Importing {executor_class} from src.experiment_design.runners.{executor_module}.")
+m = import_module(f"src.experiment_design.runners.{model_module}")
+Model = getattr(m, model_class)
 
-logger.info("Defining new class $NODE_NAME$Service(ParticipantService) to control formal name.")
+logger.info(f"Defining new class {node_name}Service(ParticipantService) to control formal name.")
 class $NODE_NAME$Service(ParticipantService):
-    ALIASES = ["$NODE_NAME$", "PARTICIPANT"]
+    ALIASES = [node_name, "PARTICIPANT"]
 
 logger.info("Constructing participant_service instance.")
 participant_service = $NODE_NAME$Service(Model, Runner)
 
-logger.info("Starting RPC server.")
+logger.info("Starting RPC server in thread.")
 server = ServerCls(participant_service, port=18861, reuse_addr=True, logger=logger, auto_register=True)
 
 def close_server_atexit():
     logger.info("Closing server due to atexit invocation.")
     server.close()
+    server_thread.join(2)
 
 def close_server_finally():
     logger.info("Closing server after 'finally' clause was reached in SERVER_SCRIPT.")
     server.close()
+    server_thread.join(2)
 
 atexit.register(close_server_atexit)
-server.start()
+server_thread = server._start_in_thread()
 
 try:
     sys.stdin.read()
@@ -98,7 +117,7 @@ class ZeroDeployedServer(DeployedServer):
                  device: dm.Device,
                  node_name: str,
                  model: tuple[str, str],
-                 runner: tuple[str, str],
+                 executor: tuple[str, str],
                  server_class="rpyc.utils.server.Server",
                  python_executable=None):
         assert device.working_cparams is not None
@@ -122,28 +141,28 @@ class ZeroDeployedServer(DeployedServer):
         script = (tmp / "deployed-rpyc.py")
         modname, clsname = server_class.rsplit(".", 1)
         m_module, m_class = model
-        sch_module, sch_class = runner
+        exr_module, exr_class = executor
         observer_ip = utils.get_local_ip()
         participant_host = device.working_cparams.host
         script.write(
             SERVER_SCRIPT.replace(    # type: ignore
-                "$MODULE$", modname
+                "$SVR-MODULE$", modname
             ).replace(
-                "$SERVER$", clsname
+                "$SVR-CLASS$", clsname
             ).replace(
                 "$MOD-MODULE$", m_module
             ).replace(
                 "$MOD-CLASS$", m_class
             ).replace(
-                "$SCH-MODULE$", sch_module
+                "$EXR-MODULE$", exr_module
             ).replace(
-                "$SCH-CLASS$", sch_class
+                "$EXR-CLASS$", exr_class
             ).replace(
-                "$NODE_NAME$", node_name
+                "$NODE-NAME$", node_name
             ).replace(
-                "$OBSERVER_IP$", observer_ip
+                "$OBS-IP$", observer_ip
             ).replace(
-                "$PARTICIPANT_HOST$", participant_host
+                "$PRT-HOST$", participant_host
             )
         )
 
