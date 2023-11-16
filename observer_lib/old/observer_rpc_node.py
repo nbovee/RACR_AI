9fe85ce3246c54125f67370ac7d611a5cfd6d521
nbovee <12849851+nbovee@users.noreply.github.com>
2023-11-16 00:48:47 -0500

    Zerodeploy merge (#8)
    * Backdoor test (#5)
    
    * quick tuneing
    
    * remainder of backdoor commits for comparison
    
    * revert forced time scaling from estimated test
    
    * remove emergency logging functions
    
    * debug pass
    
    * readme clarity
    
    * improve readability of basic test
    
    * Update README.md
    
    oops
    
    * fix SSH connection logic
    
    * zero-deploy RPC working for basic tasks
    
    * custom implementation of rpyc.zerodeploy.DeployedServer
    
    Big change in directory structure to fit better with the zerodeploy experiment setup process. An experiment is a list of node-types mapped to physical devices available on the network. A node-type is really just an RPyC service that will get zerodeployed to the physical device.
    
    * Change directory structure and add demo directory.
    
    * Progress on basic demo
    
    * Observer exposes a torch Dataset object defined by user
    
    * BaseDataLoader implements all required methods except __next__ and __init__
    
    * commit before push
    
    * About to delete some files
    
    * Reorganize directories
    
    * Edit gitignore to leave the TestResults dir empty
    
    * small edits to WrappedModel to work with thread-safe dict
    
    * Runner looking good
    
    * Task definitions outlined
    
    * Runner and Task classes solid
    
    * Fix outdated documentation
    
    * Fix zerodeploy script (no longer include DataRetriever or $ROLE$)
    
    * Update gitignore
    
    * change directory structure and fix imports
    
    * fix gitignore
    
    * start implementing the AlexNet test case using the new system (finally)
    
    * Decouple Delegator vs. Executor
    
    * Delegator-Executor relationship defined
    
    * Clean commit before integrating the new api
    
    * Add remote logger to participant servers
    
    * Fix local handler for remote log records
    
    * Apart from saving test results as csv, rough implementation done
    
    * Just on a bughunt now
    
    * fix zerodeploy remote script
    
    * clean commit
    
    * absorb Runners into NodeServices
    
    The node apis are much easier to work with if the delegator/executor functionality just becomes part of the NodeService classes
    
    * gitignore a test directory
    
    * user no longer has to subclass BaseExecutor to define participant node behavior
    
    * Experiment class uses threading.Event objects to orchestrate nodes
    
    * Fix thread logic
    
    * Add a self-destruct event for zerodeployed nodes
    
    * Pretty sure it's just a matter of fixing the remote loggers now
    
    * Remote logging works!
    
    * checkpoint
    
    * AM meeting push
    
    * mother of god it just worked!
    
    * Checkpoint after meeting with Nick
    
    Have to collect logs locally on each remote device to capture stdout and stderr
    
    * checkpoint
    
    * relocated forward exit point
    
    * unpack split input internally
    
    * Add ipynb for last report
    
    * checkpoint before fixing masterdict bottleneck
    
    Simple tests are working reliably now, but still slow. I want to try to fix the bottleneck so we can run larger tests.
    
    * masterdict bottleneck is fixed!
    
    Somewhat successful test on a larger dataset, and much faster. Just need to fix the way data is consolidated to the final report dataframe.
    
    * Testbed is working. Here's a good checkpoint commit.
    
    * Hardcoded some values for test - needs updating
    
    ---------
    
    Co-authored-by: S Piccolo <stevepic95@gmail.com>
    Co-authored-by: Stephen <piccol24@students.rowan.edu>

---
+++ observer_lib/old/observer_rpc_node.py
@@ -0,0 +1,10 @@
+import rpyc
+# from rpyc.utils.registry import RegistryServer
+
+
+class ObserverService(rpyc.Service):
+
+    def exposed_inference_completed_signal(self, uuid):
+        # to be implemented by Steve
+        print(f"callback for {uuid} triggered.")
+        pass
